c7b27589c7d7af6b5a1ba66a8d4fc27483e7e1c9
SabirMamurov <sabirmamurov@users.noreply.github.com>
2026-04-10 08:46:12 +0700

    Allow both dev and production server IPs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  allowedDevOrigins: ["192.168.242.112"],
+  allowedDevOrigins: ["192.168.242.206", "192.168.242.112"],
 };
 
 export default nextConfig;
